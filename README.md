ddb5edf1dcfdd8e4709a2f15ea2468765c619552
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-04-14 13:13:50 -0300

    export clone_repos and document on README

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Julia Organization Manager. Broken. Untested. Possibly damaging to your repo.
 
 If it's not docummented below, it's because I haven't reviewed it in a long time.
+
+## Usage
+
+**clone** repos from your org.
+
+```julia
+julia> org = "MyOrg"
+julia> my_repos = GitHub.repos(org)[1]
+julia> clone_repos(my_repos)
+```
--- src/clone.jl
+++ src/clone.jl
@@ -1,11 +1,28 @@
-function clone_repos(org::String = "JuliaSmoothOptimizers"; repo_list = repos(org)[1])
-  for r in repo_list
-    name, url = r.name, r.html_url
-    if isdir(name)
-      println("$name is already downloaded")
-      continue
+export clone_repos
+
+"""
+    clone_repos(repo_list; workdir = "workdir", exclude_non_julia = true)
+
+Clone the repos given by `repo_list` to the folder `workdir`.
+A possible value for `repo_list` is `GitHub.repos(org)[1]`.
+If `exclude_non_julia`, then if the language of the repo is not Julia, does not clone.
+Useful to avoid sites, for instance.
+"""
+function clone_repos(repo_list; workdir = "workdir", exclude_non_julia = true)
+  isdir(workdir) || mkdir(workdir)
+  cd(workdir) do
+    for r in repo_list
+      name, url = r.name, r.html_url
+      if exclude_non_julia && r.language != "Julia"
+        @warn "Ignoring $name because it's language is not Julia"
+        continue
+      end
+      if isdir(name)
+        @warn "$name is already downloaded"
+        continue
+      end
+      @info "Cloning $name"
+      run(`git clone $url`)
     end
-    println("Cloning $name")
-    run(`git clone $url`)
   end
 end
